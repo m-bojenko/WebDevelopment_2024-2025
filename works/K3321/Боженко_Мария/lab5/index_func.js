b182b061f99a012e3b87ce0ae9639222d5add51f
Marie <masha_bojenko@mail.ru>
2025-03-19 11:06:44 +0300

    added css and js

---
+++ works/K3321/Боженко_Мария/lab5/index_func.js
@@ -0,0 +1,26 @@
+function alertFunction() {
+    alert("Вы попали в болото!");
+}
+
+function showButton() {
+    const button = document.getElementById("click_me");
+
+    if (button.style.display == "none") {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+function changeBackground() {
+    const currentColor = document.body.style.backgroundColor;
+    if (currentColor == "red") {
+        document.body.style.backgroundColor = "white";
+    } else {
+        document.body.style.backgroundColor = "red";
+    }
+}
+
+function backToMap() {
+	window.location.href='index.html';
+}
